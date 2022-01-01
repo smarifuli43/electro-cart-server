9974e881d46a052d1e2f20fe8bfeb52746770197
Md Ariful Islam <smarifulislami43@gmail.com>
2022-01-02 00:22:10 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qtlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected');
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Electro cart server running');
+});
+
+app.listen(port, () => {
+  console.log(`port running at`, port);
+});
