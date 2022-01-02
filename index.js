e446293ab872ead55e769858b0b6f3f68f20df58
Md Ariful Islam <smarifulislami43@gmail.com>
2022-01-02 16:38:48 +0600

    get specific product

--- index.js
+++ index.js
@@ -11,21 +11,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qtlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
 async function run() {
   try {
     await client.connect();
     console.log('database connected');
-    
+
+    const database = client.db('Electro-cart-server');
+    const productsCollection = database.collection('products');
+
+    // get specific product
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
   } finally {
     // await client.close();
   }
